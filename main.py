590d9801ca4e20534e7ec47425f07d64ff3f96ee
gitek11 <gitek11@protonmail.com>
2023-04-17 21:43:01 +0200

    added some buttons

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from random import randint
 
 window = Tk()
 window.title("Are you dumb?")
@@ -6,13 +7,22 @@ window.geometry("500x500")
 window.resizable(0,0)
 window.iconbitmap("questhead")
 
+
+
+
+
 background = PhotoImage(file=r"background.gif")
 
 bg_label = Label(window, image=background)
 bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-text_dumb = Label(window, text = "Are you DUMB?", font=("consolas", 50, "bold"))
+text_dumb = Label(window, text = "Are you DUMB?", bg="white", font=("consolas", 50, "bold"))
 text_dumb.place(x=7, y=40)
 
+btn_yes = Button(window, text="yes", font=("consolas", 30))
+btn_yes.place(x=30, y=350)
+
+btn_no = Button(window, text="no😒", font=("consolas", 30))
+btn_no.place(x=150, y=350)
 
 window.mainloop()
