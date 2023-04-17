79a14b2c4b708b2c3749e2aa6ffd5f8a641e1279
gitek11 <gitek11@protonmail.com>
2023-04-17 21:45:24 +0200

    fixed a bug

--- main.py
+++ main.py
@@ -28,10 +28,10 @@ bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 text_dumb = Label(window, text = "Are you DUMB?", bg="white", font=("consolas", 50, "bold"))
 text_dumb.place(x=7, y=40)
 
-btn_yes = Button(window, text="yes", font=("consolas", 30))
+btn_yes = Button(window, text="yes", font=("consolas", 30), command=if_yes)
 btn_yes.place(x=30, y=350)
 
-btn_no = Button(window, text="no😒", font=("consolas", 30))
+btn_no = Button(window, text="no😒", font=("consolas", 30), command=if_no)
 btn_no.place(x=150, y=350)
 
 window.mainloop()
