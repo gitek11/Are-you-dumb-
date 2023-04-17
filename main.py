034cf9700137ddc06f36f5e8752a250bb9ed3351
gitek11 <gitek11@protonmail.com>
2023-04-17 21:31:53 +0200

    a text - "Are you DUMB?"

--- main.py
+++ main.py
@@ -11,4 +11,8 @@ background = PhotoImage(file=r"background.gif")
 bg_label = Label(window, image=background)
 bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
+text_dumb = Label(window, text = "Are you DUMB?", font=("consolas", 50, "bold"))
+text_dumb.place(x=7, y=40)
+
+
 window.mainloop()
