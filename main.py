69b1fc02d97023fdd4554e1dc145cadf401e1b66
gitek11 <gitek11@protonmail.com>
2023-04-17 21:23:54 +0200

    a background

--- main.py
+++ main.py
@@ -6,4 +6,9 @@ window.geometry("500x500")
 window.resizable(0,0)
 window.iconbitmap("questhead")
 
+background = PhotoImage(file=r"background.gif")
+
+bg_label = Label(window, image=background)
+bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+
 window.mainloop()
